99146709fb387957b7609dfec4ce6e1116de7bb3
talhaqqq <talhaqqq@gmail.com>
2017-05-03 22:10:34 +0530

    carousel done

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+    model: function() {
+      return Ember.RSVP.hash({
+      quotes: this.store.findAll('quote'),
+      backgroundimages:this.store.findAll('backgroundimage')
+    });
+    },
+
+  setupController: function(controller ,model) {
+    controller.set('quotes',model.quotes );
+    controller.set('backgroundimages',model.backgroundimages );
+
+
+
+
+  }
 });
